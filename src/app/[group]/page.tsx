b3699fbaea4018ecf1f96edaa197fb7b94b850dc
Zul Faza Makarima <zulfazamakarima@gmail.com>
2024-02-08 11:56:54 +0700

    fix not found handler

--- src/app/[group]/page.tsx
+++ src/app/[group]/page.tsx
@@ -1,6 +1,6 @@
 import { db } from "@/lib/firebase";
 import { doc, getDoc } from "firebase/firestore";
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 import Home from "../page";
 import { initGoogle } from "@/lib/google";
 
@@ -8,9 +8,6 @@ async function getData(groupId: string) {
     try {
         const docRef = doc(db, 'invitations', groupId)
         const docSnap = await getDoc(docRef);
-        if (!docSnap.exists()) {
-            throw new Error("Data not found");
-        }
         return docSnap.data()
     } catch (e) {
         console.log(e)
@@ -21,6 +18,11 @@ async function getData(groupId: string) {
 
 export default async function HomeWithGroup({ params }: { params: { group: string } }) {
     const data = await getData(params.group);
+
+    if (!data) {
+        return redirect('/');
+    }
+
     return Home({
         params: {
             group: params.group,
